bb34156779d1461613487b303945b5c5be75549d
Mubarek Ahmed <mubarekahmed44@gmail.com>
2024-12-03 21:34:20 +0300

    Add large files

--- app/page.js
+++ app/page.js
@@ -6,7 +6,11 @@ export default function Home() {
        <h2 className="text-3xl mb-4">
           Inventory Management Software
        </h2>
-       <a href="/dashboard/home/overview">View Dashboard</a>
+       <a href="/dashboard/home/overview" className="text-white bg-blue-700 
+                hover:bg-blue-800 focus:ring-4 focus:outline-none 
+                focus:ring-blue-300 font-medium rounded-lg text-sm px-5 
+                py-2.5 text-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 
+                dark:focus:ring-blue-800 inline-flex items-center">View Dashboard</a>
     </div>
   );
 }
